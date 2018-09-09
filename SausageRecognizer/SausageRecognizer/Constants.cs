27f49458938a6228ff2ac531eeae3ee9c89312b4
Arnaud MAICHAC <maichac.a@sfeir.com>
2018-09-09 14:22:21 +0200

    demo1

---
+++ SausageRecognizer/SausageRecognizer/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SausageRecognizer
+{
+    public static class Constants
+    {
+        public static string CustomVisionPredictionKey = "";
+
+        public static string CustomVisionProjectId = "";
+    }
+}
---
+++ SausageRecognizer/SausageRecognizer/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using MvvmHelpers;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace SausageRecognizer.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private MediaFile file;
+
+        private ImageSource imageSource;
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return imageSource;
+            }
+            set
+            {
+                SetProperty(ref imageSource, value);
+            }
+        }
+
+        public ICommand TakePhotoCommand { get; }
+        public ICommand PickPhotoCommand { get; }
+        public ICommand PredictCommand { get; }
+
+        private readonly PredictionEndpoint predictionEndpoint;
+
+        public MainViewModel()
+        {
+            TakePhotoCommand = new Command(async () =>
+            {
+                file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                DisplayPhoto();
+            });
+
+            PickPhotoCommand = new Command(async () =>
+            {
+                file = await CrossMedia.Current.PickPhotoAsync();
+
+                DisplayPhoto();
+            });
+
+            predictionEndpoint = new PredictionEndpoint
+            {
+                ApiKey = Constants.CustomVisionPredictionKey
+            };
+
+            PredictCommand = new Command(async () =>
+            {
+                IsBusy = true;
+
+                var result = await predictionEndpoint.PredictImageAsync(Guid.Parse(Constants.CustomVisionProjectId), file.GetStream());
+
+                string message = string.Join(Environment.NewLine,
+                    result.Predictions
+                        .OrderByDescending(p => p.Probability)
+                        .Select(p => $"{p.TagName} : {p.Probability * 100}%"));
+
+                await Application.Current.MainPage.DisplayAlert("Information", message, "OK");
+
+                IsBusy = false;
+            });
+        }
+
+        private void DisplayPhoto()
+        {
+            if (file == null)
+                return;
+                
+            ImageSource = ImageSource.FromStream(file.GetStream);
+        }
+    }
+}
